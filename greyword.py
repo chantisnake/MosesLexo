f9ee9cb12b9f5d778546f78401d628171b7d2244
chantisnake <13501393281@163.com>
2015-06-01 12:04:00 -0400

    change the function of greyword.py

--- greyword.py
+++ greyword.py
@@ -1,12 +1,14 @@
 # -*- coding:utf-8 -*-
+from __future__ import division
 from extra import loadstastic, creatdendro
+from copy import deepcopy
 from numpy.random import choice
 from collections import defaultdict
 import operator
 import os
 
 
-def greyword(Contents, inittestrounds=100, significance=0.5):
+def greyword(Contents):
     # load statics
     Len = len(Contents)
     WordLists = []
@@ -15,6 +17,7 @@ def greyword(Contents, inittestrounds=100, significance=0.5):
         wordlist = loadstastic(DendroContent)
         WordLists.append(wordlist)
         ChunkSizes.append(sum(wordlist.values()))
+    Result = deepcopy(WordLists)
 
     # find largest file
     Max = 0
@@ -22,69 +25,48 @@ def greyword(Contents, inittestrounds=100, significance=0.5):
         if Max < size:
             Max = size
 
-    # create geryword and dendrogram for inittestround times
-    DendroNum = {}
-    FileStore = defaultdict()
-    for _ in range(inittestrounds):
-        print 'the', _+1, 'times to test'
-        # creates a deep copy of the lists
-        tempWordLists = WordLists[:]
-        tempChunkSizes = ChunkSizes[:]
-        tempContents = Contents[:]
+    # create geryword and dendrogram
+    for i in range(Len):  # focus on a file to normalize
+        Diff = Max - ChunkSizes[i]
+        NumGreyWord = Diff / (Len-1)  # not use itself
 
-        for i in range(Len):  # focus on a file to normalize
-            Diff = Max - ChunkSizes[i]
-            NumGreyWord = Diff / Len
-
-            for j in range(Len):  # choose a file to pick grey word
-                wordlist = Contents[j].split()
-
-                for _ in range(NumGreyWord):  # fill in the word
-                    word = choice(wordlist)  # randomly choose a word from wordlist
-                    tempContents[i] += ' ' + word
-                    tempChunkSizes[i] += 1  # update chunksize
-                    # update WordLists
+        for j in range(Len):  # choose a file to pick grey word
+            if j != i and NumGreyWord != 0:  # not it self, or do not need to add
+                for word in WordLists[j].keys():
+                    # add word to the chunk
                     try:
-                        tempWordLists[i][word] += 1
+                        Result[i][word] += WordLists[j][word]*(NumGreyWord/ChunkSizes[j])
                     except KeyError:
-                        tempWordLists[i].update({word: 1})
-        dendro = creatdendro(tempWordLists, tempChunkSizes)['leaves']
-        dendro = tuple(dendro)  # convert the dendro into a tuple type
-        # update DendroNum and FileStore
-        try:
-            DendroNum[dendro] += 1
-            FileStore[dendro].append(tempContents)
-        except KeyError:
-            DendroNum.update({dendro: 1})
-            FileStore.update({dendro: [tempContents]})
+                        Result[i].update({word: WordLists[j][word]*(NumGreyWord/ChunkSizes[j])})
 
-    # find the most common dendrogram
-    ResultDendro = max(DendroNum.iteritems(), key=operator.itemgetter(1)) #[0]
-    DendroContent = FileStore[ResultDendro[0]]
-    return ResultDendro, DendroContent
+    dendro = creatdendro(WordLists, ChunkSizes)['leaves']
+    return Result, dendro
 
 
 if __name__ == "__main__":
-    count = 0
-    for _ in range(100):
-        print 'hello'
-        # read from 'TestSuite' folder
-        path = os.path.join(os.getcwd(), 'TestSuite')
-        data = {}
-        for dir_entry in os.listdir(path):
-            dir_entry_path = os.path.join(path, dir_entry)
-            if os.path.isfile(dir_entry_path):
-                with open(dir_entry_path, 'r') as my_file:
-                    data[dir_entry] = my_file.read()
-        Content = data.values()
 
-        print 'content read'
-        ResultDendro, DendroContent = greyword(Content, inittestrounds=1000)
-        print 'calculation done'
-        if ResultDendro[1] != 1:
-            print 'different'
-            count += 1
-    print(count)
+    print 'hello'
+    # read from 'TestSuite' folder
+    path = os.path.join(os.getcwd(), 'TestSuite')
+    data = {}
+    for dir_entry in os.listdir(path):
+        dir_entry_path = os.path.join(path, dir_entry)
+        if os.path.isfile(dir_entry_path):
+            with open(dir_entry_path, 'r') as my_file:
+                data[dir_entry] = my_file.read()
+    Content = data.values()
+
+    print 'content read'
+    newWordLists, bingmeiyouniaoyong = greyword(Content)
+    print
+    for wordlist in newWordLists:
+        for key in wordlist.keys():
+            wordlist[key] = round(wordlist[key])
+    for wordlist in newWordLists:
+        print wordlist
+        print sum(wordlist.values())
+
+
 
 
     '''
